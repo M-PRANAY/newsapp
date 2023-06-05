348a3b8a0b3d1de541f78cf93eb39421da046695
Pranay Kumar <pranaykumarmolleti@gmail.com>
2023-06-05 22:30:15 +0530

    V-23

---
+++ src/Components/News.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Newsitem from '../Newsitem'
+
+export default function News() {
+  return (
+    <div>
+      News Components
+      <Newsitem />
+      <Newsitem />
+      <Newsitem />
+      <Newsitem />
+    </div>
+  )
+}
